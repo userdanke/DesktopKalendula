b50704cecd828e6d4838b5b733d8f53613ce5c49
Ledys2312 <lmaradiagas2425@politecnics.barcelona>
2025-10-20 19:26:16 +0200

    Creación de plantilla de interfaz.

---
+++ DiseñoForms.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopKalendula
+{
+    public class DiseñoForms : Panel
+    {
+
+        private Button btnCerrar;
+        private Button btnMinimizar;
+        private Color[] colores = {
+        Color.FromArgb(211, 145, 109),
+        Color.FromArgb(229, 122, 122),
+        Color.FromArgb(204, 163, 193),
+        Color.FromArgb(228, 235, 241) };
+
+
+
+        public DiseñoForms()
+        {
+ 
+            this.Dock = DockStyle.Top;
+            this.Height = 40;
+            this.Paint += BarraColores_Paint; ;
+
+            btnCerrar = new Button();
+            btnCerrar.Text = "✕";
+            btnCerrar.FlatStyle = FlatStyle.Flat;
+            btnCerrar.BackColor = Color.Transparent;
+            btnCerrar.ForeColor = Color.FromArgb(61, 23, 0);
+            btnCerrar.Font = new Font("Microsoft Sans Serif", 10, FontStyle.Bold);
+            btnCerrar.FlatAppearance.BorderSize = 0;
+            btnCerrar.Size = new Size(30, 30);
+            btnCerrar.Click += (s, e) => Application.Exit(); 
+
+            btnMinimizar = new Button();
+            btnMinimizar.Text = "⎯";
+            btnMinimizar.FlatStyle = FlatStyle.Flat;
+            btnMinimizar.BackColor = Color.Transparent;
+            btnMinimizar.ForeColor = Color.FromArgb(61, 23, 0);
+            btnMinimizar.FlatAppearance.BorderSize = 0;
+            btnMinimizar.Size = new Size(30, 30);
+            btnMinimizar.Click += (s, e) => this.FindForm().WindowState = FormWindowState.Minimized;
+
+            this.Controls.Add(btnCerrar);
+            this.Controls.Add(btnMinimizar);
+
+            this.Resize += DiseñoForms_Resize;
+        }
+
+        private void DiseñoForms_Resize(object sender, EventArgs e)
+        {
+
+            btnCerrar.Location = new Point(this.Width - btnCerrar.Width - 5, 5);
+            btnMinimizar.Location = new Point(this.Width - btnCerrar.Width - btnMinimizar.Width - 10, 5);
+        }
+
+        private void BarraColores_Paint(object sender, PaintEventArgs e)
+        {
+            int ancho = this.Width / colores.Length;
+            for (int i = 0; i < colores.Length; i++)
+            {
+                Rectangle rect = new Rectangle(i * ancho, 0, ancho, this.Height);
+                using (Brush b = new SolidBrush(colores[i]))
+                {
+                    e.Graphics.FillRectangle(b, rect);
+                }
+            }
+        }
+    }
+}
--- LoginPage.cs
+++ LoginPage.cs
@@ -16,27 +16,9 @@ namespace DesktopKalendula
         {
             InitializeComponent();
 
-            Panel barraColores = new Panel();
-            barraColores.Dock = DockStyle.Top;
-            barraColores.Height = 40;
-            barraColores.Paint += BarraColores_Paint;
-            this.Controls.Add(barraColores);
+            DiseñoForms diseño = new DiseñoForms();
+            this.Controls.Add(diseño);
 
-            Button btnCerrar = new Button();
-            btnCerrar.Text = "X";
-            btnCerrar.FlatStyle = FlatStyle.Flat;
-            btnCerrar.Size = new Size(30, 30);
-            btnCerrar.Location = new Point(barraColores.Width - 25, 5);
-            btnCerrar.Click += (s, e) => this.Close();
-            barraColores.Controls.Add(btnCerrar);
-
-            Button btnMinimizar = new Button();
-            btnMinimizar.Text = "-";
-            btnMinimizar.FlatStyle = FlatStyle.Flat;
-            btnMinimizar.Size = new Size(30, 30);
-            btnMinimizar.Location = new Point(barraColores.Width - 85, 5);
-            btnMinimizar.Click += (s, e) => this.WindowState = FormWindowState.Minimized;
-            barraColores.Controls.Add(btnMinimizar);
         }
 
         private void LoginPage_Load(object sender, EventArgs e)
@@ -49,22 +31,6 @@ namespace DesktopKalendula
 
         }
 
-        private void BarraColores_Paint(object sender, PaintEventArgs e)
-        {
-            Panel panel = sender as Panel;
-            int ancho = panel.Width / 4; 
-
-            Color[] colores = { Color.FromArgb(211,145,109), Color.FromArgb(229, 122, 122), Color.FromArgb(204, 163, 193), Color.FromArgb(228, 235, 241) };
-
-            for (int i = 0; i < 4; i++)
-            {
-                Rectangle rect = new Rectangle(i * ancho, 0, ancho, panel.Height);
-                using (Brush b = new SolidBrush(colores[i]))
-                {
-                    e.Graphics.FillRectangle(b, rect);
-                }
-            }
-        }
     }
 
 }
