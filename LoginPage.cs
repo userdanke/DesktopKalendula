5657cebf3b357f9f0b8d9f800b4189e431a914ca
Ledys2312 <lmaradiagas2425@politecnics.barcelona>
2025-10-20 18:44:09 +0200

    Creación de plantilla grafiica de todos los forms.

--- LoginPage.cs
+++ LoginPage.cs
@@ -17,10 +17,26 @@ namespace DesktopKalendula
             InitializeComponent();
 
             Panel barraColores = new Panel();
-            barraColores.Dock = DockStyle.Top; // Se mantiene arriba
-            barraColores.Height = 40; // Altura de la barra
-            barraColores.Paint += BarraColores_Paint; // Evento para dibujar colores
+            barraColores.Dock = DockStyle.Top;
+            barraColores.Height = 40;
+            barraColores.Paint += BarraColores_Paint;
             this.Controls.Add(barraColores);
+
+            Button btnCerrar = new Button();
+            btnCerrar.Text = "X";
+            btnCerrar.FlatStyle = FlatStyle.Flat;
+            btnCerrar.Size = new Size(30, 30);
+            btnCerrar.Location = new Point(barraColores.Width - 25, 5);
+            btnCerrar.Click += (s, e) => this.Close();
+            barraColores.Controls.Add(btnCerrar);
+
+            Button btnMinimizar = new Button();
+            btnMinimizar.Text = "-";
+            btnMinimizar.FlatStyle = FlatStyle.Flat;
+            btnMinimizar.Size = new Size(30, 30);
+            btnMinimizar.Location = new Point(barraColores.Width - 85, 5);
+            btnMinimizar.Click += (s, e) => this.WindowState = FormWindowState.Minimized;
+            barraColores.Controls.Add(btnMinimizar);
         }
 
         private void LoginPage_Load(object sender, EventArgs e)
@@ -36,9 +52,8 @@ namespace DesktopKalendula
         private void BarraColores_Paint(object sender, PaintEventArgs e)
         {
             Panel panel = sender as Panel;
-            int ancho = panel.Width / 4; // Dividimos en 4 partes
+            int ancho = panel.Width / 4; 
 
-            // Colores
             Color[] colores = { Color.FromArgb(211,145,109), Color.FromArgb(229, 122, 122), Color.FromArgb(204, 163, 193), Color.FromArgb(228, 235, 241) };
 
             for (int i = 0; i < 4; i++)
