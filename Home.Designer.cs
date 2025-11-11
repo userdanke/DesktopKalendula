db03fb5433651007c739473899af4cab4c66a865
Ledys2312 <lmaradiagas2425@politecnics.barcelona>
2025-11-11 20:23:40 +0100

    pepe

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -28,20 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelName = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(792, 111);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(54, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Kalendulá";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(250)))), ((int)(((byte)(249)))));
+            this.ClientSize = new System.Drawing.Size(1648, 891);
+            this.Controls.Add(this.labelName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Home";
             this.Text = "Home";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Home_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelName;
     }
 }
